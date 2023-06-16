f3718463b9e65846478d5cddaf758edba6f3d5c7
Manoel <piloukos45@gmail.com>
2023-06-16 09:17:38 -0300

    teste

--- assets/js/header.js
+++ assets/js/header.js
@@ -5,8 +5,8 @@ const modal_sobre = document.querySelector('.modal_sobre');
 icone_nave.addEventListener('click', ()=>{
     
     if(modal_sobre.style.height === ""){
-        modal_sobre.style.height = "calc(100vh - 50px";
-        icone_nave.style.bottom = "calc(100vh - 100px";
+        modal_sobre.style.height = "calc(100vh - 100px";
+        icone_nave.style.bottom = "calc(100vh - 150px";
     }else{
         modal_sobre.style.height = ""
         icone_nave.style.bottom = '40px';
