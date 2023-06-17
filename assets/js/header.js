07d701b15f434727beb8b2fd8132db091dcbd533
Manoel <piloukos45@gmail.com>
2023-06-17 13:19:59 -0300

    ajuste reponsividade

--- assets/js/header.js
+++ assets/js/header.js
@@ -4,7 +4,8 @@ const modal_sobre = document.querySelector('.modal_sobre');
 const toggle_icone = document.querySelector('.toggle_icone');
 const toggle_dark = document.querySelector('.toggle_dark');
 const body = document.querySelector('body');
-const link = document.querySelector('.nav_bar a');
+const nav_bar = document.querySelector('.nav_bar');
+const link = document.querySelectorAll('.nav_bar a');
 const nav = document.querySelector('nav');
 const menu_mobile = document.querySelector('.menu_mobile');
 
@@ -17,22 +18,35 @@ icone_nave.addEventListener('click', ()=>{
         modal_sobre.style.height = ""
         icone_nave.style.bottom = '40px';
     }
-    setTimeout( ()=> icone_nave.classList.toggle('nave_rotate'), 600)
+
+    setTimeout( ()=> icone_nave.classList.toggle('nave_rotate'), 600);
 
 });
 
 toggle_icone.addEventListener('click', ()=>{
+   nav_bar.classList.toggle('dark_model');
     toggle_icone.classList.toggle('toggle_icone_fundo');
     body.classList.toggle('dark_model');
 
 });
 
 menu_mobile.addEventListener('click', ()=>{
-    menu_mobile.classList.toggle("menu_toggle")
+
+    menu_mobile.classList.toggle("menu_toggle");
+
     if( nav.style.right == '0px'){
         nav.style.right = '-180px'
     }else{
         nav.style.right = '0px'
     }
    
+});
+
+link.forEach((item)=>{
+    item.addEventListener("click", ()=>{
+        for(let i = 0; i < link.length; i++){
+            link[i].classList.remove('ativo');
+        }
+        item.classList.add('ativo');
+    });
 });
